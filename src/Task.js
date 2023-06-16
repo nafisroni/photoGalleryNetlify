a43b14b953f844a7bbbb41ccf7c88cc74432aff7
nafisroni <bawangm74@gmail.com>
2023-06-16 13:12:48 +0700

    Masukin First Commit

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "soft-cat-0786fc.netlify.app/" // TODO: replace this
+const studentName = "Muhammad Nafis Roni Wijaya" // TODO: replace this
+const studentId = "FE4347425" // TODO: replace this
+
+export { url, studentName, studentId }
